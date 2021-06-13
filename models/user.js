48d919f71b99874f6b540d64912584c9a6d4cfdc
amr khafagy <amr.khafagy@yahoo.com>
2021-06-13 14:25:28 +0100

    user & databd

--- models/user.js
+++ models/user.js
@@ -1,2 +1,23 @@
 const config = require('config');
 const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+     },
+     password: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 255
+    },
+    profileImage:{ type : String}
+});
