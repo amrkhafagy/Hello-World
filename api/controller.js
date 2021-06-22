eb085ca7f3e5600e21318b62b1dc860db6905175
amr khafagy <amr.khafagy@yahoo.com>
2021-06-22 19:33:08 +0100

    init

--- api/controller.js
+++ api/controller.js
@@ -110,3 +110,15 @@ exports.getdocbook = async (req,res) => {
     }
   };
   
+  exports.getbook = async (req,res) => {
+    let book = await UserServices.getallbook({user:req.params.userid});
+    if(book){
+      res.status(200).json({
+        data:book
+      })
+    }else{
+      res.status(400).json({
+        message:"Appointment Not Found",
+      });
+    }
+  };
