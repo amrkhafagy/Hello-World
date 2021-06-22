82c2d0a20c1996a6a9ea88ee409cc2c90c81d05a
amr khafagy <amr.khafagy@yahoo.com>
2021-06-22 19:30:13 +0100

    user acces

---
+++ api/controller.js
@@ -0,0 +1,68 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+
+exports.registerUser = async (req, res) => {
+    
+    var user = await UserServices.getUserByEmail({email:req.body.email});
+
+    console.log("User",user);
+    if(user) { 
+        return res.status(409).json({
+            error:"User already exists "
+        }); 
+    }
+    user = await UserServices.create(req.body);
+
+
+    res.status(201).json({
+        message: "User Created Success",
+        name: user.name,
+        email: user.email
+    });
+
+
+
+};
+
+exports.signinUser = async (req,res)=>{
+  
+    var user = await UserServices.getUserByEmail({email:req.body.email});
+  
+    if(!user){
+      return res.status(409).json({
+        error:"User Not exists "
+      }); 
+    }
+    const hash = user.password;
+
+  bcrypt.compare(req.body.password, hash, function(err, result) {
+      if(result){
+        const token= jwt.sign({
+            name:user.name,
+            email:user.email,
+            userType:'User',
+            userId: user._id
+            
+        },
+        config.get('myprivatekey')
+        ,
+       {
+          expiresIn:"1h"
+        }
+        );
+      console.log("login success "+token)
+       res.header("x-auth-token", token).status(200).json({
+           message:"login Successfuly...!!!",
+           token:token,
+           data:user
+       });
+    }else{
+
+        res.status(404).json({
+            message:"Wrong Password"
+        });
+    }
+   });
+ };
