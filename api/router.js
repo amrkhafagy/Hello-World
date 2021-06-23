ef7870ddf296a25d5b50a2c323f589bee5df353f
amr khafagy <amr.khafagy@yahoo.com>
2021-06-23 18:28:14 +0100

    bush

--- api/router.js
+++
@@ -1,3 +0,0 @@
-const express = require("express");
-const UserRouter = express.Router();
-const UserController = require('./controller');
--- api/routes.js
+++ api/routes.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const UserRouter = express.Router();
+const UserController = require('./controller');
+
+
+UserRouter.post('/signup', UserController.registerUser);
+UserRouter.post('/signin',UserController.signinUser);
+UserRouter.get('/getallbookings',UserController.getbook);
+UserRouter.get('/getalldoctors',UserController.getdoctors)
+UserRouter.get('/getallpatients',UserController.getpatients)
