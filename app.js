ae71b8348baac744226f720a2f1ed5bea8571c01
amr khafagy <amr.khafagy@yahoo.com>
2021-07-31 09:41:50 +0100

    init

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 // require imports packages required by the application
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const config = require("config");
-const mongoose = require('mongoose');
-const User = require('./models/user')
+const mongoose = require("mongoose");
+const User = require("./models/user")
 
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
  
-
- //UserRouter= require('./api/routes')
-
-
 
 app.use(cors())
 //const HOST = '127.0.0.1';
@@ -28,9 +24,16 @@ var mongoDB = config.get("uri");
 
 
 
-mongoose.connect(mongoDB, {useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true})
-.then(() => {console.log("Connected to MongoDB...")})
-  .catch(err => console.error("Could not connect to MongoDB...")
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log("Connected to MongoDB...")
+  })
+  .catch(err =>
+    console.error("Could not connect to MongoDB...")
 );
   
 
@@ -39,15 +42,15 @@ mongoose.connect(mongoDB, {useNewUrlParser: true, useCreateIndex: true,useUnifie
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(
+  console, "MongoDB connection error:"
+));
 
 
 // Allow app to support differnt body content types (using the bidyParser package)
 app.use(bodyParser.json());
-//app.use(body.json());
-//app.use(body.urlencoded({extended:false}));
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!")
 })
 
 
@@ -57,30 +60,16 @@ app.get('/', (req, res) => {
 //app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+  res.header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 
-//catch 404 and forward to error handler
-//app.use(function (req, res, next) {
 
-  //var err = new Error('Not Found: '+ req.method + ":" + req.originalUrl);
-  
-  //err.status = 404;
-  
- //next(err);
-  
- // });
+app.use("/Api/v1/user", require("./api/routes"));
+app.use("/Api/v1/doctor", require("./api/doctor.router"));
 
-
-
-
-
-app.use('/Api/v1/user', require('./api/routes'));
-app.use('/Api/v1/doctor', require('./api/doctor.router'));
-//app.use(bodyParser.json())
 
 //app.post('/api/register', async (req, res) => {
   //console.log(req.body)
